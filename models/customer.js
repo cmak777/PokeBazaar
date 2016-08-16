a22842864d616706c53520c6e1ab164b2474ca1c
Christopher Mak <christophermak777@gmail.com>
2016-08-16 19:02:27 -0400

    Hashing during registration but no verfication yet for login

--- models/customer.js
+++ models/customer.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
 var findOrCreate = require('mongoose-findorcreate');
 
 var customerSchema = mongoose.Schema({
@@ -18,5 +19,14 @@ var customerSchema = mongoose.Schema({
   }
 });
 customerSchema.plugin(findOrCreate);
+
+customerSchema.statics.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// check if password is valid
+customerSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
 
 module.exports = mongoose.model('Customer', customerSchema);
